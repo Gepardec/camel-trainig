6837fc61fbe080376014933efcc08eeada8aa31a
Egor Erofeev <egor.erofeev@gepardec.com>
2020-11-10 21:46:24 +0100

    ex5 finished

--- solutions/bestplayground/src/main/java/com/gepardec/trainings/camel/best/MyRoutes.java
+++ solutions/bestplayground/src/main/java/com/gepardec/trainings/camel/best/MyRoutes.java
@@ -4,11 +4,13 @@ import javax.inject.Inject;
 
 import org.apache.camel.Endpoint;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.cdi.ImportResource;
 import org.apache.camel.cdi.Uri;
 
 /**
  * Configures all our Camel routes, components, endpoints and beans
  */
+@ImportResource("camel-context.xml")
 public class MyRoutes extends RouteBuilder {
 
     @Inject
