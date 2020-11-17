c0d781543588af7bb9989cd933b7d0cb2047fc66
Erhard Siegl <erhard.siegl@gepardec.com>
2020-11-17 22:24:55 +0100

    Use @Handler for bean binding

--- solutions/simple-cdi/src/test/java/com/gepardec/trainings/camel/best/OrderDtoProcessor.java
+++ solutions/simple-cdi/src/test/java/com/gepardec/trainings/camel/best/OrderDtoProcessor.java
@@ -1,10 +1,18 @@
 package com.gepardec.trainings.camel.best;
 
+import org.apache.camel.Handler;
+
 public class OrderDtoProcessor {
 	
+	@Handler
 	public OrderItemDto processDto(OrderItemDto dto) {
 		dto.amount++;
 		return dto;
 	}
+
+	public String doSomething(OrderItemDto dto) {
+		return "Something";
+	}
+
 
 }
